ba9b2cf453b15a23cbe56baa6434ae2808c4f3e5
lucedeswede <nathanielcarolina@gmail.com>
2017-11-05 15:49:11 +0800

    test: Ensure URLs are defined and not empty for each feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URLs are defined and not empty', function() {
+            for (let i = 0; i < allFeeds.length; i++) {
+                let feed = allFeeds[i];
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
