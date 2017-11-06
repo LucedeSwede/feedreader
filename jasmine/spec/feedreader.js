f660fa7da7d0d5bb20036b390bd033291377e482
lucedeswede <nathanielcarolina@gmail.com>
2017-11-06 17:28:43 +0800

    test: Update test to ensure there is at least 1 .entry element in .feed container

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,7 +67,7 @@ $(function() {
         });
 
         it('on loadFeed, have at least 1 .entry element in .feed container', function(done) {
-            expect($('.feed').children().length).toBeGreaterThan(0);
+            expect($('.feed').find('.entry').length).toBeGreaterThan(0);
             done();
         });
     });
